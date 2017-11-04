131ad74ad2eee5c7f162c623c80a38db3d695371
peter0123chang <peter0123chang@gmail.com>
2017-11-04 19:19:41 +0800

    Update LineBot.js

--- api/services/LineBot.js
+++ api/services/LineBot.js
@@ -3,8 +3,8 @@ const LineRequest = require('../services/LineRequest');
 
 module.exports = {
   authentication: async (requestObj) => {
-    const channelSecret = process.env.lineChannelSecret || '';
-    const channelAccessToken = process.env.lineChannelAccessToken || '';
+    const channelSecret = process.env.lineChannelSecret || '9a8c7887c8ed7ceb3f963ce8f73d64a9';
+    const channelAccessToken = process.env.lineChannelAccessToken || '3cL4zroJybMhWDUTZPUnLbxzPjfsoNu5FO0EqmZ/fhOka8bgzPZBCCp2EkmVw4l5lRUhDRML3vSuT9wV+VUd/xzw02X6FL6nFi8LaaT8NDuJcNJaaIIyNDzB/C5dfeU21BS73yzS3jQ88fmlwQS1VAdB04t89/1O/w1cDnyilFU=';
 
     const xLineSignature = requestObj.headers['x-line-signature'];
     const requestBody = requestObj.body || {};
